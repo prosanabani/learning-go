1ce51f87ca89004e64c402f4feb7c05279dab59c
Ali Ibrahim Al-Sanabani <pro.sanabani@gmail.com>
2025-04-02 00:03:10 +0300

    Add types package import and implement ReturnJust function

--- mohammed/api/api.go
+++ mohammed/api/api.go
@@ -1,7 +1,19 @@
 package api
 
-import "github.com/prosanabani/learning-go/constants"
+import (
+	"github.com/prosanabani/learning-go/constants"
+	"github.com/prosanabani/learning-go/types"
+)
 
 func Api() string {
 	return "Api" + constants.APIPrefix
+}
+
+
+
+func ReturnJust (x  ,   y int) (types.Person)  {
+	return types.Person{ 
+		Name:  "Mohammed",
+		Age:  y,
+	}
 }
