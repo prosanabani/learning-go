d6684da0c42172d0dfc58cc39aaed0107b5702d3
Ali Ibrahim Al-Sanabani <pro.sanabani@gmail.com>
2025-04-02 00:03:16 +0300

    Implement shape interface with circle and rectangle types, and add area and circumference methods

--- main.go
+++ main.go
@@ -1,11 +1,45 @@
 package main
 
-import (
-	"fmt"
+type shape interface {
+	area() int
+	circumference() int
+}
 
-	"github.com/prosanabani/learning-go/constants"
-)
+type circle struct {
+	width, height int
+}
+type rectangle struct {
+	depth, height int
+}
+
+func (currentCircle circle) area() int {
+	return currentCircle.width * currentCircle.height
+}
+
+func (currentCircle rectangle) area() int {
+	return currentCircle.height * currentCircle.height
+}
+func (currentCircle circle) circumference() int {
+	return currentCircle.height * currentCircle.width
+}
+func (currentCircle rectangle) circumference() int {
+	return currentCircle.height * currentCircle.depth
+}
+
+func PrintArea(currentShape shape) {
+	println(currentShape.area())
+}
+
+func printCirum(currentShape shape) {
+	println(currentShape.circumference())
+}
+
 func main() {
-	fmt.Printf(constants.AppName)
-  
+
+	myCircle := circle{width: 10, height: 20}
+	myRectangle := rectangle{depth: 10, height: 20}
+
+	printCirum(myCircle)
+	printCirum(myRectangle)
+
 }
---
+++ types/types.go
@@ -0,0 +1,7 @@
+package types // Use the same package name if these files are part of the same package
+
+// Person struct definition
+type Person struct {
+    Name string
+    Age  int
+}
