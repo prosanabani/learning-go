1c13b886aeec59f0cfd8ac71cab00c1752baa533
Ali Ibrahim Al-Sanabani <pro.sanabani@gmail.com>
2025-04-03 23:37:51 +0300

    Refactor expense handling to support email and SMS types with updated cost calculation and reporting functionality

--- main.go
+++ main.go
@@ -3,34 +3,55 @@ package main
 import "fmt"
 
 type expense interface {
-	cost(rate int) (expenseCost float64)
+	cost() float64
 }
 
-type formatter interface {
-	print() (PrintedString string)
+func getExpenseReport(e expense) (text string, cost float64) {
+	email, isEmail := e.(email)
+	
+	if isEmail {
+		return email.toAddress, email.cost()
+	}
+	sms, isSms := e.(sms)
+	if isSms {
+		
+		return sms.toPhoneNumber, sms.cost()
+	}
+
+	return  "", 0.0 
+
 }
 
 type email struct {
 	isSubscribed bool
 	body         string
+	toAddress    string
 }
 
-func (e email) cost(rate int) float64 {
-	if e.isSubscribed {
-		return float64(len(e.body)) * float64(rate)
+type sms struct {
+	isSubscribed  bool
+	body          string
+	toPhoneNumber string
+}
 
+type invalid struct{}
+
+func (e email) cost() float64 {
+	if !e.isSubscribed {
+		return float64(len(e.body)) * .05
 	}
-	return float64(len(e.body)) * float64(rate)
+	return float64(len(e.body)) * .01
 }
 
-func (e email) print() string {
-	return e.body
+func (s sms) cost() float64 {
+	if !s.isSubscribed {
+		return float64(len(s.body)) * .1
+	}
+	return float64(len(s.body)) * .03
 }
 
-func PrintEmailDetails(e expense, f formatter , rate int) {
-	fmt.Println("Email Details:")
-	fmt.Println("Cost:", e.cost(rate))
-	fmt.Println("Body:", f.print())
+func (i invalid) cost() float64 {
+	return 0.0
 }
 
 func main() {
@@ -38,9 +59,15 @@ func main() {
 	myEmail := email{
 		isSubscribed: true,
 		body:         "kjvdnwjfdhv",
+		toAddress:    "sdkjdnbkrje",
+	}
+	mySms := sms{
+		isSubscribed:  true,
+		body:          "kjvdnwjfdhv",
+		toPhoneNumber: "986451",
 	}
 
+	fmt.Println(getExpenseReport(myEmail))
+	fmt.Println(getExpenseReport(mySms))
 
-
-	PrintEmailDetails(myEmail, myEmail,  985)
 }
