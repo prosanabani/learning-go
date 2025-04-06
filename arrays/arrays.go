e424d026c63dbb5284620aeb8e65a23b0dbc3d2d
Ali Ibrahim Al-Sanabani <pro.sanabani@gmail.com>
2025-04-07 00:30:23 +0300

    arrays practicing

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -18,14 +18,23 @@ const (
 
 // }
 
-func modifySth(just []int) []int {
-	just[0] = 88888
-	return just
+// func modifySth(just []int) []int {
+// 	just[0] = 88888
+// 	return just
+// }
+
+func variaticFunc(text string, numbers ...int) string {
+	sum := 0
+	for number := 0; number < len(numbers); number++ {
+		sum += numbers[number]
+	}
+	return fmt.Sprintf("%s %d", text, sum)
 }
 
 func Main() {
-
-	// freePlans, err := getMessageWithRetriesForPlan(planFree, [3]string{"message1", "message2", "message3"})
+	fmt.Println(variaticFunc("hello", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+	// arrayToSend := [3]string{"message1", "message2", "message3"}
+	// freePlans, err := getMessageWithRetriesForPlan(planFree, arrayToSend)
 	// if err != nil {
 	// 	fmt.Println(err)
 	// 	return
@@ -33,7 +42,7 @@ func Main() {
 
 	// fmt.Println(freePlans)
 
-	// proPlans, err := getMessageWithRetriesForPlan(planPro, [3]string{"message1", "message2", "message3"})
+	// proPlans, err := getMessageWithRetriesForPlan(planPro, arrayToSend)
 	// if err != nil {
 	// 	fmt.Println(err)
 	// 	return
@@ -41,7 +50,7 @@ func Main() {
 
 	// fmt.Println(proPlans)
 
-	// errorPlans, err := getMessageWithRetriesForPlan("kdsjnvd", [3]string{"message1", "message2", "message3"})
+	// errorPlans, err := getMessageWithRetriesForPlan("kdsjnvd", arrayToSend)
 	// if err != nil {
 	// 	fmt.Println(err)
 	// 	return
@@ -50,8 +59,20 @@ func Main() {
 
 	// fmt.Println(errorPlans)
 
-	just_array := [5]int{1, 2, 3, 4, 5}
-	slice := just_array[0:4]
-	fmt.Println(modifySth(slice)) // pass the slice of the array
-	fmt.Println(just_array)
+	// just_array := [5]int{1, 2, 3, 4, 5}
+	// slice := just_array[0:4]
+	// fmt.Println(modifySth(slice)) // pass the slice of the array
+	// fmt.Println(just_array)
+
+	// sliceArray := make([]int, 5, 10)
+	// sliceArray[0] = 1
+	// sliceArray[1] = 2
+	// sliceArray[2] = 3
+	// sliceArray[3] = 4
+	// sliceArray[4] = 5
+
+	// sliceArray = append(sliceArray, 6, 7, 8, 9, 10, 11) // append to the slice
+	// fmt.Println(len(sliceArray))
+	// fmt.Println(cap(sliceArray))
+
 }
